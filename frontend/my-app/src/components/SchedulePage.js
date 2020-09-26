de9fc3dd7f2a95795436e50a488d46265f116d0f
element2562 <element25668@gmail.com>
2020-09-26 11:32:06 -0500

    Have booking page looking pretty good so far. Having some issues with validating the fields. Probably going to skip that for now and focus on functionality then come back to it. Just need to write up the API calls for updating the slot that's being booked, and the create patient. Going to create patient THEN update slot so we're able to have a patient record attached to the slot

--- frontend/my-app/src/components/SchedulePage.js
+++ frontend/my-app/src/components/SchedulePage.js
@@ -1,10 +1,29 @@
-import React, { Component } from "react";
-import { Form } from "react-bootstrap";
+import React, { Component, useState } from "react";
+import { Form, Button } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 class SchedulePage extends Component {
     state = {
-        slot: {}
+        slot: {},
+        validated: true
     }
+    
+    setValidated = (isValidated) => {
+        this.setState({
+            validated: isValidated
+        })
+    }
+
+    handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+    
+        this.setValidated(true);
+    };
+    
+
     userFriendlyDate = (startTime, endTime) => {
         let startTimeArr = startTime.split("T");
         let date = startTimeArr[0];
@@ -36,11 +55,56 @@ class SchedulePage extends Component {
                     <p><strong>Doctor Location: </strong>{doctor.address}, {doctor.zip}</p>
                     <p><strong>Doctor Phone Number: </strong>{doctor.phone}</p>
                 </div>
-                <Form>
-                    <Form.Group controlId="patientFirstName">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control placeholder="John" />
-                    </Form.Group>
+                <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+                    <div id="patientLineOne">
+                        <Form.Group controlId="patientFirstName">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control required placeholder="John" />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid first name.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="patientLastName">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control required placeholder="Doe" />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid last name.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="patientDOB">
+                            <Form.Label>Date of birth</Form.Label>
+                            <Form.Control required type="date" />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid date of birth.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </div>
+                    <div id="patientLineTwo">
+                        <Form.Group controlId="patientAddress">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control required placeholder="123 Main st City, State Zip" />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid address.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="patientAddress">
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control required type="tel" placeholder="111-111-1111" isInvalid="test" />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid phone number.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="patientEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control required type="email" placeholder="testEmail@test.com" />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid email.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </div>
+                    <div id="submitAppointmentButton">
+                        <Button>Book Appointment</Button>
+                    </div>
                 </Form>
             </div>
         )
